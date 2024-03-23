d24c06c486279d564e63e9b10f97ae4610f133fe
Silas  Kumi <silaskumi4@gmail.com>
2024-03-23 23:01:29 +0000

    migrate

--- Glazers/settings.py
+++ Glazers/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     #third-party
